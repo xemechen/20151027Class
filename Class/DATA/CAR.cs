7640310a9fcbd5a19371d4cbe2ca9fde4325d519
XemeChen <xemechen@hotmail.com>
2015-10-27 20:26:00 +0800

    Class of Objective class

---
+++ Class/DATA/CAR.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibrary.DATA
+{
+    public class CAR
+    {
+
+        private string name;
+        private int year;
+
+        public void SetName(string name)
+        {
+            this.name=name;
+        }
+
+
+        public void SetYear(int year)
+        {if (year >= 2015)
+                this.year = 2015;
+            else if (year <= 1850)
+                this.year = 1850;
+            else
+                this.year = year;}
+
+
+        public string GetName()
+        {
+        return this.name;
+        }
+
+
+        public int GetYear()
+        {
+            return this.year;
+        }
+
+
+
+        public int Year
+        {
+            get { return this.year; }
+
+            set
+            {
+                if (value >= 2015)
+                    this.year = 2015;
+                else if (value <= 1850)
+                    this.year = 1850;
+                else
+                    this.year = value;
+            }
+        }
+            
+    }
+}
---
+++ Example01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarLibrary.DATA;
+
+namespace Example01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CAR ccCar = new CAR();
+            Owner ccOwner = new Owner();
+
+            ccCar.SetName("AXeme");
+            int year = int.Parse(Console.ReadLine());
+
+            ccCar.SetYear(year);
+            Console.WriteLine(ccCar.GetYear());
+
+            ccCar.Year = year;
+            Console.WriteLine(ccCar.Year);
+        
+        
+        }
+    }
+}
